786310e6e102ae7102d206d29cdef533715b99e8
Win Pa <winpasandar1985@gmail.com>
2023-07-07 21:44:06 +0630

    Added GitHub Repo URL.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/Win-PaSandar/react-weather-app"
+          target="_blank"
+        >
+          Open-source code
+        </a>{" "}
+        by Win-Pa Sandar
+      </footer>
+    </div>
+  );
 }

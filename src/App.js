f728f2151328ee8fabf62d9bcac16dc9c80f2772
Win Pa <winpasandar1985@gmail.com>
2023-07-07 22:07:53 +0630

    Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/Win-PaSandar/react-weather-app"
-          target="_blank"
-        >
-          Open-source code
-        </a>{" "}
-        by Win-Pa Sandar
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/Win-PaSandar/react-weather-app"
+            target="_blank"
+          >
+            Open-source code
+          </a>{" "}
+          by Win-Pa Sandar
+        </footer>
+      </div>
     </div>
   );
 }
